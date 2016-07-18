6b8a9716b3b52989623a76d959855a51bc664f75
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-07-18 07:24:45 +0200

    Report the Accept header for json responses

--- Core/Classes/Networking/JsonHttpClient.swift
+++ Core/Classes/Networking/JsonHttpClient.swift
@@ -1,13 +1,28 @@
 import Foundation
+import RxSwift
 import SwiftyJSON
 
-class JsonHttpClient: HttpClient<JSON> {
+public class JsonHttpClient: HttpClient<JSON> {
 
-    
     // MARK: - Init
     
     public init(requestDispatcher: UrlRequestDispatcher = UrlRequestDispatcher(), sessionAdapter: Adapter<NSURLRequest, NSURLRequest>? = nil) {
         super.init(responseAdapter: UrlJsonResponseAdapter(), requestDispatcher: requestDispatcher, sessionAdapter: sessionAdapter)
+    }
+    
+    override public func request(request: NSURLRequest) -> Observable<(JSON, NSURLResponse?)> {
+        return super.request(self.requestWithJSONHeaders(request))
+    }
+    
+    
+    // MARK: - Private
+    
+    private func requestWithJSONHeaders(request: NSURLRequest) -> NSURLRequest {
+        let mutableRequest: NSMutableURLRequest = request.mutableCopy() as! NSMutableURLRequest
+        var headers = mutableRequest.allHTTPHeaderFields ?? [:]
+        headers["Accept"] = "application/json"
+        mutableRequest.allHTTPHeaderFields = headers
+        return mutableRequest.copy() as! NSURLRequest
     }
     
 }
