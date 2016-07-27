a98e3fcef6c9a0fcff3b9e75b6040abffc01ce18
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-07-27 19:51:38 +0200

    Add a public constructor to Oauth2Session

--- Core/Classes/Networking/Oauth2/Oauth2Session.swift
+++ Core/Classes/Networking/Oauth2/Oauth2Session.swift
@@ -3,6 +3,11 @@ import Foundation
 public struct Oauth2Session: Equatable {
     public let accessToken: String
     public let refreshToken: String
+
+    public init(accessToken: String, refreshToken: String) {
+      self.accessToken = accessToken
+      self.refreshToken = refreshToken
+    }
 }
 
 public func ==(lhs: Oauth2Session, rhs: Oauth2Session) -> Bool {
