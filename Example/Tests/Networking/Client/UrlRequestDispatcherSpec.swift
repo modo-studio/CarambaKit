65e39b5a1fbcd3e2bd10317ab74b4ab710a9c794
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-09-24 11:27:19 +0200

    [swift-3.0] Re-enable tests and fix the travis build script

--- Example/Tests/Networking/Client/UrlRequestDispatcherSpec.swift
+++ Example/Tests/Networking/Client/UrlRequestDispatcherSpec.swift
@@ -18,41 +18,40 @@ class UrlRequestDispatcherSpec: QuickSpec {
             OHHTTPStubs.removeAllStubs()
         }
         
-        //FIXME
-//        describe("-dispatch:") {
-//            context("when there's an error") {
-//                it("should send an error event to the observer") {
-//                    let error = NSError(domain: "", code: -1, userInfo: nil)
-//                    _ = stub(condition: isScheme("test"), response: { (request) -> OHHTTPStubsResponse in
-//                        return OHHTTPStubsResponse(error: error)
-//                    })
-//                    let request = URLRequest(url: NSURL(string: "test://test")! as URL)
-//                    waitUntil(action: { (done) in
-//                        let disposable = subject.dispatch(request: request)
-//                            .subscribe(onError: { (_error) in
-//                                done()
-//                            })
-//                    })
-//                }
-//            }
-//            context("when there's data") {
-//                it("should send the next event with the data to the observer") {
-//                    let data = Data()
-//                    let request = URLRequest(url: URL(string: "test://test")!)
-//                    _ = stub(condition: isScheme("test"), response: { (request) -> OHHTTPStubsResponse in
-//                        return OHHTTPStubsResponse(data: data, statusCode: 200, headers: nil)
-//                    })
-//                    waitUntil(action: { (done) in
-//                        let disposable = subject.dispatch(request: request)
-//                            .do(onCompleted: { 
-//                                done()
-//                            })
-//                            .subscribe(onNext: { (input) in
-//                                expect(input.data) == data
-//                            })
-//                    })
-//                }
-//            }
-//        }
+        describe("-dispatch:") {
+            context("when there's an error") {
+                it("should send an error event to the observer") {
+                    let error = NSError(domain: "", code: -1, userInfo: nil)
+                    _ = stub(condition: isScheme("test"), response: { (request) -> OHHTTPStubsResponse in
+                        return OHHTTPStubsResponse(error: error)
+                    })
+                    let request = URLRequest(url: NSURL(string: "test://test")! as URL)
+                    waitUntil(action: { (done) in
+                        _ = subject.dispatch(request: request)
+                            .subscribe(onError: { (_error) in
+                                done()
+                            })
+                    })
+                }
+            }
+            context("when there's data") {
+                it("should send the next event with the data to the observer") {
+                    let data = Data()
+                    let request = URLRequest(url: URL(string: "test://test")!)
+                    _ = stub(condition: isScheme("test"), response: { (request) -> OHHTTPStubsResponse in
+                        return OHHTTPStubsResponse(data: data, statusCode: 200, headers: nil)
+                    })
+                    waitUntil(action: { (done) in
+                        _ = subject.dispatch(request: request)
+                            .do(onCompleted: { 
+                                done()
+                            })
+                            .subscribe(onNext: { (input) in
+                                expect(input.data) == data
+                            })
+                    })
+                }
+            }
+        }
     }
 }
